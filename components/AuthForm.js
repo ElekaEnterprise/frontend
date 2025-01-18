fb2af7d6ef8f40083fad29402fd7711f0c322bdd
joel-kalema <joelkalema63@gmail.com>
2025-01-18 03:44:29 +0200

    updated the from design

--- components/AuthForm.js
+++ components/AuthForm.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { auth } from '../utils/firebaseConfig';
 import {
@@ -21,11 +22,11 @@ export default function AuthForm({ isSignUp = false }) {
       if (isSignUp) {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         console.log('Signed up:', userCredential.user);
-        router.push('/form');  // Redirect to form page
+        router.push('/form');
       } else {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
         console.log('Logged in:', userCredential.user);
-        router.push('/form');  // Redirect to form page
+        router.push('/form');
       }
     } catch (error) {
       console.error('Authentication error:', error.message);
@@ -37,7 +38,7 @@ export default function AuthForm({ isSignUp = false }) {
     try {
       const result = await signInWithPopup(auth, provider);
       console.log('Google Sign-In successful:', result.user);
-      router.push('/form');  // Redirect to form page
+      router.push('/form'); 
     } catch (error) {
       console.error('Google Sign-In error:', error.message);
     }
@@ -48,32 +49,63 @@ export default function AuthForm({ isSignUp = false }) {
     try {
       const result = await signInWithPopup(auth, provider);
       console.log('GitHub Sign-In successful:', result.user);
-      router.push('/form');  // Redirect to form page
+      router.push('/form');
     } catch (error) {
       console.error('GitHub Sign-In error:', error.message);
     }
   };
 
   return (
-    <div>
-      <h2>{isSignUp ? 'Sign Up' : 'Log In'}</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleAuth}>{isSignUp ? 'Sign Up' : 'Log In'}</button>
-      <div>
-        <button onClick={googleSignIn}>Sign in with Google</button>
-        <button onClick={githubSignIn}>Sign in with GitHub</button>
+    <div className='h-[100vh] flex justify-center items-center'>
+    <div className="max-w-md mx-auto mt-10 p-6 bg-[#16213b] shadow-lg rounded-lg">
+      <Image src='/logo.png' height={70} width={80} alt="Eleka logo" className='mx-auto -mt-10 mb-10'/>
+      <h2 className="text-2xl font-semibold mb-6 text-center">{isSignUp ? 'Sign Up' : 'Log In'}</h2>
+      <p className='w-5/6 mx-auto text-center mb-12'>Hey, Enter your details to get <span>{isSignUp ? 'Sign Up' : 'Log In'}</span> to your account!</p>
+      <div className="mb-4">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full px-4 py-2 bg-[#1a2744] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
       </div>
+      <div className="mb-10">
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full px-4 py-2 bg-[#1a2744] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <button
+        onClick={handleAuth}
+        className="w-full py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-transform transform"
+      >
+        {isSignUp ? 'Sign Up' : 'Log In'}
+      </button>
+
+      <div className='my-10 flex gap-4 items-center justify-center'>
+        <div className='w-6 bg-[#9CA3AF] h-1'></div>
+        <p>Or <span>{isSignUp ? 'Sign Up' : 'Log In'}</span> With</p>
+        <div className='w-6 bg-[#9CA3AF] h-1'></div>
+      </div>
+        <button
+          onClick={googleSignIn}
+          className="flex justify-between items-center gap-4 mx-auto p-2 pr-4 bg-red-500 text-white rounded-md hover:bg-red-600 transition-transform transform"
+        >
+          <Image src='/google.png' height={35} width={35} alt="Eleka logo" className='rounded-md'/>
+          <p>Sign in with Google</p>
+        </button>
+        {/* <button
+          onClick={githubSignIn}
+          className="py-2 px-4 bg-gray-800 text-white rounded-md hover:bg-gray-900 transition-transform transform"
+        >
+          Sign in with GitHub
+        </button> */}
+      {/* </div> */}
+    </div>
     </div>
   );
 }
